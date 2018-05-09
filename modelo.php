9f06d404151b18b81401ae52e2fcb09fb814be80
Ainhoa de la Hucha <ainhoadelahucha@gmail.com>
2018-05-09 16:26:48 +0200

    login y registro

--- modelo.php
+++ modelo.php
@@ -1,8 +1,8 @@
 <?php
 
 	function conectarbasedatos() {
-		//$mysql = mysqli_connect("localhost","root","","siw");
-		$mysql = mysqli_connect("dbserver","grupo15","ohsoebiaxe","db_grupo15");
+		$mysql = mysqli_connect("localhost","root","","siw");
+		//$mysql = mysqli_connect("dbserver","grupo15","ohsoebiaxe","db_grupo15");
 		return $mysql;
 	}
 
